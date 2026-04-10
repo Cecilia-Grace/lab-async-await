3956d169f1a1c1e949a5afde27c76354576b9743
Cecilia Grace <cecigrace2003@gmail.com>
2026-04-11 02:37:50 +0300

    added .js code

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code here!
+const API_URL = 'https://jsonplaceholder.typicode.com/posts'
+
+const postList = document.querySelector('#post-list')
+
+function displayPosts(postsArray) {
+    postList.innerHTML = ''
+
+    postsArray.forEach(item => {
+        const posts_li = document.createElement('li')
+
+        const posts_head = document.createElement('h1')
+        posts_head.textContent = item.title
+
+        const posts_para = document.createElement('p')
+        posts_para.textContent = item.body
+
+        posts_li.append(posts_head, posts_para)
+        postList.appendChild(posts_li)
+    })
+}
+
+async function houseAndFetchPosts() {
+    const response = await fetch(API_URL)
+
+    const data = await response.json()
+
+    displayPosts(data)
+}
+
+houseAndFetchPosts()
